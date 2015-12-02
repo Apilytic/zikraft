f5d2dcd11b1d423fb3e84a69dd1ed3538d30c007
gogoluxecs <georgi.lambov@gmail.com>
2015-12-03 01:27:48 +0200

    Dependencies; log and text as html.

---
+++ dev.py
@@ -0,0 +1,34 @@
+import cherrypy
+import sys, os
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('templates'))
+
+class Root(object):
+    @cherrypy.expose
+    @cherrypy.tools.gzip()
+    def index(self):
+	tmpl = env.get_template('index.html')
+	return tmpl.render(salutation='Hello', target='World')
+
+
+if __name__ == '__main__':
+     conf = {
+         '/': {
+             'tools.sessions.on': True,
+             'tools.staticdir.root': os.path.abspath(os.getcwd())
+         },
+         '/css': {
+             'tools.staticdir.on': True,
+             'tools.staticdir.dir': './static/css'
+         },
+         '/js': {
+             'tools.staticdir.on': True,
+             'tools.staticdir.dir': './static/js'
+         },
+         '/images': {
+             'tools.staticdir.on': True,
+             'tools.staticdir.dir': './static/images'
+         },
+
+     }
+     cherrypy.quickstart(Root(), '/', config=conf)
