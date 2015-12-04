eec32517fa461dbea1dca45de9296a9d42264b1f
gogoluxecs <georgi.lambov@gmail.com>
2015-12-04 10:39:07 +0200

    center bottom for large desktops with js.

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+var FooterPosition = {
+
+    e : $("#body-text"),
+    documentWidth : $(document).width(),
+
+    init : function() {
+        if (this.documentWidth >= 1200) {
+            this.centerLargeDesktop();
+        }
+
+        this.e.show();
+    },
+
+    centerLargeDesktop : function() {
+        this.e.css('left', (this.documentWidth - this.e.width()) / 2);
+
+    }
+};
+
+FooterPosition.init();
