45e3f39a06a9dc7cd627b1ad441b485e56a1c9e4
gogoluxecs <georgi.lambov@gmail.com>
2015-12-04 12:25:41 +0200

    scale logo by scale ratio.

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,47 @@
-var FooterPosition = {
+var ElementScaler = {
     e: $("#body-text"),
+    logo: $("#logo"),
     documentWidth: $(document).width(),
+    documentHeight: $(document).height(),
+    baseLogo: {
+        'width': 379,
+        'height': 110
+    },
 
     init: function () {
         if (this.documentWidth >= 1200) {
-            this.centerLargeDesktop();
+            this.centerFooter();
         }
 
         this.e.show();
+        this.scaleLogo();
     },
 
-    centerLargeDesktop: function () {
+    centerFooter: function () {
         this.e.css('left', (this.documentWidth - this.e.width()) / 2);
+    },
 
+    scaleLogo: function () {
+        if ((this.documentWidth / this.documentHeight) > (window.screen.width / window.screen.height)) {
+            this.logo.css('width', this.documentWidth * 0.32);
+
+            var xScaleFactor = this.baseLogo.width / this.logo.width();
+            var yScaleFactor = this.baseLogo.height / this.logo.height();
+            var scaleFactor = Math.min(xScaleFactor, yScaleFactor);
+
+            this.logo.css('padding-top', 103 * scaleFactor - 19);
+        }
     }
 };
 
-FooterPosition.init();
+ElementScaler.init();
 
 $(window).resize(function () {
-    FooterPosition.init();
+    ElementScaler.init();
+
 });
+
+setTimeout(function () {
+    //$("#logo img").show();
+
+}, 1000);
